534fc97e3360e03ef3616943eaa4ad3392d9a123
ghani-zaman <ghaniuz.zaman@salsoft.net>
2024-09-04 22:11:28 +0500

    production

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Sample Nodejs App');
+  res.send('Production');
 });
 
 app.get('/users', (req, res) => {
