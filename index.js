06657062892c74f905d2565d137dfee949dd37da
ghani-zaman <ghaniuz.zaman@salsoft.net>
2024-09-04 21:38:52 +0500

    prod

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Sample Nodejs App');
+});
+
+app.get('/users', (req, res) => {
+  const users = require('./user-data.json');
+  res.send(users);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
