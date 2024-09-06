2c801f0569b7a01f72f43b4e1129500945a05177
ghani-zaman <ghaniuz.zaman@salsoft.net>
2024-09-06 16:25:59 +0500

    staging environment

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Staging');
+  res.send('Staging Environment');
 });
 
 app.get('/users', (req, res) => {
